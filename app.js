48c297ef52c90c1dfe887f0a2bfa3f998bf10708
Sami Khan <76680018+samikhan805@users.noreply.github.com>
2021-07-06 10:19:28 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// addEventListener
+// querySelector
+
+var container = document.querySelector('.container')
+var section = document.querySelector('section')
+function main() {
+  container.classList.toggle('active')
+  section.classList.toggle('active')
+}
